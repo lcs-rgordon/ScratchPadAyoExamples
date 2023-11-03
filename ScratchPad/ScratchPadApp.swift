fd1ad2e447eb5e161c1a168e887903fc9191db62
Russell Gordon <rgordon@lcs.on.ca>
2023-11-03 06:36:54 -0400

    Initial Commit

---
+++ ScratchPad/ScratchPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchPadApp.swift
+//  ScratchPad
+//
+//  Created by Russell Gordon on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct ScratchPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
